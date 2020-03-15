d47c6ec45c7652d30dea3a6dc1b75fa436884966
AmoDinho <u14284783@tuks.co.za>
2020-03-15 20:39:03 +0200

    ADDED puesdo code

--- lunar-tour-api/src/resolvers/mutation.js
+++ lunar-tour-api/src/resolvers/mutation.js
@@ -2,6 +2,19 @@ import { v1 as uuidv1 } from "uuid"
 import * as dynamoDBLib from "../../libs/dynamodb-lib";
 
 export const makeABooking = async (args, context) => {
+    /*
+    How to get all the listings?
+    
+    Get the listing ID
+    Query the db for the listing
+    get it's price
+    
+    options? 
+    
+    := do a scan for a listing
+    := get all the listings/ only get their prices and then filter only if the price has the relveant listing ID
+    */
+
 
     const params = {
         TableName: process.env.BookingsDB,
