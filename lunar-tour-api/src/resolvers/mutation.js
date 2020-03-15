acbf6dc08206b38a896fe49d0c1bfbb4e7a9d23d
AmoDinho <u14284783@tuks.co.za>
2020-03-15 20:00:34 +0200

    done with mutation

--- lunar-tour-api/src/resolvers/mutation.js
+++ lunar-tour-api/src/resolvers/mutation.js
@@ -2,5 +2,31 @@ import uuid from "uuid"
 import * as dynamoDBLib from "../../libs/dynamodb-lib";
 
 export const makeABooking = async (args, context) => {
-    return null
+    const params = {
+        TableName: process.env.BookingsDB,
+        Item: {
+            bookingsId: uuid.v1(),
+            listingId: "a114dded-ddef-4052-a106-bb18b94e6b51",
+            bookingDate: Date.now(),
+            size: args.size,
+            bookingTotal: 33 * args.size,
+            customerEmail: args.customerEmail,
+            customers: [{
+                name: args.name,
+                Surname: args.Surname,
+                country: args.country,
+                passportNumber: args.passportNumber,
+                physioScore: args.physioScore
+            }]
+        }
+
+    }
+
+    try {
+        await dynamoDBLib.call("put", params)
+        return true
+    } catch (e) {
+        return e
+    }
+
 }
