b80c9e5e08bbaabdfe554ffdea7437e143c858d8
AmoDinho <u14284783@tuks.co.za>
2020-03-22 21:30:34 +0200

    created a landing header

--- lunar-tour-client/src/components/navigation/Nav.vue
+++ lunar-tour-client/src/components/navigation/Nav.vue
@@ -3,7 +3,7 @@
     class="border-top flex items-center justify-between flex-wrap bg-transparent p-6"
   >
     <div class="flex items-center flex-no-shrink text">
-      <h1 class="font-display text-red">
+      <h1 class="font-display text-red text-2xl">
         <a class="no-underline text-red" href="/">Lunar</a>
       </h1>
     </div>
