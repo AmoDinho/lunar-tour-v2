e2413f138d5fd5dafcf3c0206e5cafd08db1a164
AmoDinho <u14284783@tuks.co.za>
2020-03-22 22:39:11 +0200

    WTF

--- lunar-tour-client/src/pages/Home.vue
+++ lunar-tour-client/src/pages/Home.vue
@@ -1,12 +1,41 @@
 <template>
-  <LandingHeader />
+  <div>
+    <LandingHeader />
+    <ApolloQuery
+      :query="
+        gql => gql`
+          query getAllListings {
+            listingId
+            coverPhoto
+            listingName
+            listingLocation
+            rating
+            price
+          }
+        `
+      "
+    >
+      <template v-slot="{ result: { loading, error, data } }">
+        <div v-if="loading">loading...</div>
+        <div v-if="error">error...</div>
+
+        <div v-else-if="data">
+          <div v-for="listing of data.getAllListings" :key="listing.listingId">
+            {{ listing.listingName }}
+          </div>
+        </div>
+      </template>
+    </ApolloQuery>
+  </div>
 </template>
 <script>
 import LandingHeader from "../components/headers/LandingHeader.vue";
+//import TourCard from "../components/cards/TourCard";
 export default {
   name: "Home",
   components: {
     LandingHeader
+    //TourCard
   }
 };
 </script>
