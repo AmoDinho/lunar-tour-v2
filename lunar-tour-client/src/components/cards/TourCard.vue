48dae596cf46e5c2dde6e1e827e8d37eea2c8b99
AmoDinho <u14284783@tuks.co.za>
2020-03-22 21:58:31 +0200

    creaed tour card

---
+++ lunar-tour-client/src/components/cards/TourCard.vue
@@ -0,0 +1,12 @@
+<template> </template>
+<script>
+export default {
+  name: "Tour Card",
+  props: {
+    listingTitle: String,
+    listingLocation: String,
+    price: String,
+    rating: String
+  }
+};
+</script>
