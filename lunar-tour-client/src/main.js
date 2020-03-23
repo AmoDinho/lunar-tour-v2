635d47d8b97e2f820be56138baa15753e6786cff
AmoDinho <u14284783@tuks.co.za>
2020-03-23 19:04:11 +0200

    added the router

--- lunar-tour-client/src/main.js
+++ lunar-tour-client/src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import "./assets/css/tailwind.css";
 import { createProvider } from "./vue-apollo";
 Vue.config.productionTip = false;
+import router from "./router";
 
 new Vue({
+  router,
   apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
