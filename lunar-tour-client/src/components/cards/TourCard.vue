37145376733e8c3d571ec4e4647b73b18c75df5b
AmoDinho <u14284783@tuks.co.za>
2020-03-23 18:39:15 +0200

    added a cursor

--- lunar-tour-client/src/components/cards/TourCard.vue
+++ lunar-tour-client/src/components/cards/TourCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rounded-px w-card h-card shadow-lg-card">
+  <div class="rounded-px w-card h-card shadow-lg-card cursor-pointer">
     <img :src="coverPhoto" class="w-full h-48 rounded-t-px" />
     <div class="flex flex-col p-2">
       <div class="flex flex-row mt-2 ">
