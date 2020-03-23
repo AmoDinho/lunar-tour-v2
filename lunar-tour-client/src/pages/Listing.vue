fab17b67b63fe9eb2766306a4a9bdd5b1e9772ec
AmoDinho <u14284783@tuks.co.za>
2020-03-23 20:23:12 +0200

    done with this

--- lunar-tour-client/src/pages/Listing.vue
+++ lunar-tour-client/src/pages/Listing.vue
@@ -1,28 +1,55 @@
 <template>
   <div>
-    <ApolloQuery
-      :query="require('../graphql/getAListing.gql')"
-      :variables="{ listingId: $route.query.id }"
-    >
-      <template v-slot="{ result: { loading, error, data } }">
-        <!-- Loading -->
-        <div v-if="loading" class="loading apollo text-red">Loading...</div>
-
-        <!-- Error -->
-        <div v-else-if="error" class="error apollo text-red">
-          An error occurred
-        </div>
-
-        <!-- Result -->
-        <div v-else-if="data" class="result apollo text-red">
-          {{ data.getAListing.listingName }}
-        </div>
-      </template>
-    </ApolloQuery>
+    <div class="text-red">{{ listing.getAListing.listingName }}</div>
   </div>
 </template>
 <script>
+// import GET_A_LISTING from "../graphql/getAListing.gql";
+import gql from "graphql-tag";
+
 export default {
-  name: "Listing"
+  name: "Listing",
+  data() {
+    return {
+      listing: {}
+    };
+  },
+  apollo: {
+    listing: {
+      query: gql`
+        query GetAListing($listingId: String) {
+          getAListing(listingId: $listingId) {
+            coverPhoto
+            listingId
+            listingName
+            specialType
+          }
+        }
+      `,
+      variables: {
+        listingId: "6fb8fcbd-7ed3-45b6-b2ba-5bb99d7024e2"
+      }
+    }
+  }
 };
+
+//   <ApolloQuery
+//       :query="require('')"
+//       :variables="{ listingId: $route.query.id }"
+//     >
+//       <template v-slot="{ result: { loading, error, data } }">
+//         <!-- Loading -->
+//         <div v-if="loading" class="loading apollo text-red">Loading...</div>
+
+//         <!-- Error -->
+//         <div v-else-if="error" class="error apollo text-red">
+//           An error occurred
+//         </div>
+
+//         <!-- Result -->
+//         <div v-else-if="data" class="result apollo text-red">
+//           {{ data.getAListing.listingName }}
+//         </div>
+//       </template>
+//     </ApolloQuery>
 </script>
