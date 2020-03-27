42690b4e7df8109f50fc1b6a91edc3570e17fbde
AmoDinho <u14284783@tuks.co.za>
2020-03-27 19:38:06 +0200

    added antd

--- lunar-tour-client/src/main.js
+++ lunar-tour-client/src/main.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Progress from "ant-design-vue/lib/progress";
+import Tabs from "ant-design-vue/lib/tabs";
+import "ant-design-vue/dist/antd.css";
+
 import VueRouter from "vue-router";
 import "./assets/css/tailwind.css";
 import { createProvider } from "./vue-apollo";
 Vue.config.productionTip = false;
 import router from "./router";
 Vue.use(VueRouter);
+
+Vue.component(Tabs);
+Vue.component(Progress);
 
 new Vue({
   router,
