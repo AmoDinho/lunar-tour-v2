6142d8090048aae9652ec43cdb731f718f674da3
AmoDinho <u14284783@tuks.co.za>
2020-03-27 19:54:49 +0200

    got antd working

--- lunar-tour-client/src/pages/booking/Index.vue
+++ lunar-tour-client/src/pages/booking/Index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    HI BOOKING
+    <a-tabs defaultActiveKey="1">
+      <a-tab-pane tab="Tab 1" key="1">Tab 1</a-tab-pane>
+      <a-tab-pane tab="Tab 2" disabled key="2">Tab 2</a-tab-pane>
+      <a-tab-pane tab="Tab 3" key="3">Tab 3</a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 
