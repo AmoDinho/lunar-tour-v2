95d5d8b73d3874aa1e8e8228bfd2c41940283c11
AmoDinho <u14284783@tuks.co.za>
2020-03-29 21:13:49 +0200

    added back buttons

--- lunar-tour-client/src/pages/booking/Index.vue
+++ lunar-tour-client/src/pages/booking/Index.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
-    <a-tabs defaultActiveKey="1">
+    <a-tabs v-model="activeKey">
       <a-tab-pane tab="Tab 1" key="1">
+        page 1
         <input placeholder="date" type="date" />
 
         <input placeholder="3" type="number" />
 
         <input placeholder="email" type="email" />
+        <button @click="next(2)">next</button>
       </a-tab-pane>
-      <a-tab-pane tab="Tab 2" key="2">Tab 2</a-tab-pane>
-      <a-tab-pane tab="Tab 3" key="3">Tab 3</a-tab-pane>
+      <a-tab-pane tab="Tab 2" key="2" class="text-black"
+        >Page 2
+        <input placeholder="3" type="number" />
+
+        <input placeholder="email" type="email" />
+        <button @click="next(2)">back</button>
+
+        <button @click="next(3)">next</button>
+      </a-tab-pane>
+      <a-tab-pane tab="Tab 3" key="3">Page 3</a-tab-pane>
+      <button @click="next(2)">back</button>
     </a-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Index"
+  name: "Index",
+  data() {
+    return {
+      activeKey: "1"
+    };
+  },
+  methods: {
+    next(k) {
+      this.activeKey = k.toString();
+    }
+  }
 };
 </script>
 <style>
