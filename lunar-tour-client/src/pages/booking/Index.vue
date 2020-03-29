eff8d6ba720d4781d601f212bfe471302c3f222e
AmoDinho <u14284783@tuks.co.za>
2020-03-29 20:42:34 +0200

    added fields for the first tab

--- lunar-tour-client/src/pages/booking/Index.vue
+++ lunar-tour-client/src/pages/booking/Index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <a-tabs defaultActiveKey="1">
-      <a-tab-pane tab="Tab 1" key="1">Tab 1</a-tab-pane>
-      <a-tab-pane tab="Tab 2" disabled key="2">Tab 2</a-tab-pane>
+      <a-tab-pane tab="Tab 1" key="1">
+        <input placeholder="date" type="date" />
+
+        <input placeholder="3" type="number" />
+
+        <input placeholder="email" type="email" />
+      </a-tab-pane>
+      <a-tab-pane tab="Tab 2" key="2">Tab 2</a-tab-pane>
       <a-tab-pane tab="Tab 3" key="3">Tab 3</a-tab-pane>
     </a-tabs>
   </div>
@@ -13,3 +19,8 @@ export default {
   name: "Index"
 };
 </script>
+<style>
+.ant-tabs-bar.ant-tabs-top-bar {
+  display: none;
+}
+</style>
