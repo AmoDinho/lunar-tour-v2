eba651d8861f71c65b32b45e164cdd63e4f35278
AmoDinho <u14284783@tuks.co.za>
2020-03-30 20:34:53 +0200

    created RemoveButton

---
+++ lunar-tour-client/src/components/buttons/RemoveButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    @click="handleClick"
+    class="bg-white  text-red-darkest focus:outline-none font-display  px-16 py-2 cursor:pointer"
+  >
+    <div class="flex flex-row">
+      <p class="text-sm font-display text-bold ">Remove</p>
+    </div>
+  </button>
+</template>
+<script>
+export default {
+  name: "RemoveButton",
+  methods: {
+    handleClick(evt) {
+      this.$emit("click", evt);
+    }
+  }
+};
+</script>
