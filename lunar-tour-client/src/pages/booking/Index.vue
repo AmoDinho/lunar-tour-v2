0a68550462a5c35a6758f15382c5640f4ca095e9
AmoDinho <u14284783@tuks.co.za>
2020-03-31 20:51:12 +0200

    got most of it fixed

--- lunar-tour-client/src/pages/booking/Index.vue
+++ lunar-tour-client/src/pages/booking/Index.vue
@@ -37,8 +37,12 @@
                   v-model="email"
                 />
               </div>
-              <div class="flex flex-row mt-5">
-                <RedBlockButton @click="next(2)" text="Proceed" class="mr-5" />
+              <div class="flex lg:flex-row mt-5 s:flex-col">
+                <RedBlockButton
+                  @click="next(2)"
+                  text="Proceed"
+                  class="mr-5 s:mb-5 lg:mb-0"
+                />
                 <RedOutlineButton @click="$router.push('/')" text="Cancel" />
               </div>
             </div>
@@ -107,8 +111,12 @@
               <div class="mt-5 ">
                 <BlueBlockButton text="Add a customer" @click="addCustomer" />
               </div>
-              <div class="flex flex-row mt-5">
-                <RedBlockButton @click="next(3)" text="Proceed" class="mr-5" />
+              <div class="flex lg:flex-row mt-5 s:flex-col">
+                <RedBlockButton
+                  @click="next(3)"
+                  text="Proceed"
+                  class="mr-5 s:mb-5  lg:mb-0"
+                />
                 <RedOutlineButton @click="next(1)" text="Back" />
               </div>
             </div>
