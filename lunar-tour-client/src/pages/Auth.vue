f31373f9904640bde8b367b7df6305f0588e77f5
AmoDinho <u14284783@tuks.co.za>
2020-04-10 21:52:27 +0200

    setup auth page

--- lunar-tour-client/src/pages/Auth.vue
+++ lunar-tour-client/src/pages/Auth.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <SignUp />
+    <SignUp :toggle="toggle" v-if="formState === 'signUp'" />
+    <SignIn v-if="formState === 'signIn'" />
+    <p @click="toggle">
+      {{ formState === "signUp" ? "Already signed up?" : "Need an account?" }}
+    </p>
   </div>
 </template>
 <script>
 import SignUp from "../components/auth/SignUp";
+import SignIn from "../components/auth/SignIn";
+
 export default {
   name: "Auth",
   components: {
-    SignUp
+    SignUp,
+    SignIn
+  },
+  data() {
+    return {
+      formState: "signUp"
+    };
+  },
+  methods: {
+    toggle() {
+      this.formState === "signUp"
+        ? (this.formState = "signIn")
+        : (this.formState = "signUp");
+    }
   }
 };
 </script>
