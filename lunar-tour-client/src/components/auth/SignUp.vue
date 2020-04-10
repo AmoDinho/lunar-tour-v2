cb444c96897ee3a933c6b5e3f4fc3933d1a0fb68
AmoDinho <u14284783@tuks.co.za>
2020-04-10 21:11:11 +0200

    added form basics

--- lunar-tour-client/src/components/auth/SignUp.vue
+++ lunar-tour-client/src/components/auth/SignUp.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
-    sign up
+    <HeadingOne>
+      Sign up
+    </HeadingOne>
+    <div>
+      <Input placeholder="email" />
+      <Input placeholder="password" type="password" />
+      <RedBlockButton text="Sign Up" />
+    </div>
   </div>
 </template>
 <script>
+import HeadingOne from "../typography/HeadingOne";
+// import BodyOne from "../components/typography/BodyOne";
+import Input from "../components/inputs/Input";
+ import RedBlockButton from "../components/buttons/RedBlockButton";
+// import RedOutlineButton from "../components/buttons/RedOutlineButton";
 export default {
-  name: "Auth"
+  name: "Auth",
+  components: {
+    HeadingOne
+    // BodyOne,
+    // DateInput,
+Input,
+    RedBlockButton,
+    // RedOutlineButton,
+    // RemoveButton,
+    // BlueBlockButton,
+    // Card
+  }
 };
 </script>
--- lunar-tour-client/src/pages/Auth.vue
+++ lunar-tour-client/src/pages/Auth.vue
@@ -1,29 +1,8 @@
 <template>
-  <div>
-    <HeadingOne>
-      Sign up
-    </HeadingOne>
-  </div>
+  <div></div>
 </template>
 <script>
-import HeadingOne from "../components/typography/HeadingOne";
-// import BodyOne from "../components/typography/BodyOne";
-// import DateInput from "../components/inputs/DateInput";
-// import Input from "../components/inputs/Input";
-// import RedBlockButton from "../components/buttons/RedBlockButton";
-// import RedOutlineButton from "../components/buttons/RedOutlineButton";
 export default {
-  name: "Auth",
-  components: {
-    HeadingOne
-    // BodyOne,
-    // DateInput,
-    // Input,
-    // RedBlockButton,
-    // RedOutlineButton,
-    // RemoveButton,
-    // BlueBlockButton,
-    // Card
-  }
+  name: "Auth"
 };
 </script>
