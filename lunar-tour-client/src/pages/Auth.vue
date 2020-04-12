7e5fb6b2dfd9dad0be819ea1554d01938887dffb
AmoDinho <u14284783@tuks.co.za>
2020-04-12 22:10:20 +0200

    done styling auth

--- lunar-tour-client/src/pages/Auth.vue
+++ lunar-tour-client/src/pages/Auth.vue
@@ -3,21 +3,22 @@
     <SignUp :toggle="toggle" v-if="formState === 'signUp'" />
     <SignIn v-if="formState === 'signIn'" />
     <div class="flex justify-center mt-10">
-      <p @click="toggle">
+      <BodyOne @click.native="toggle" class="cursor-pointer">
         {{ formState === "signUp" ? "Already signed up?" : "Need an account?" }}
-      </p>
+      </BodyOne>
     </div>
   </div>
 </template>
 <script>
 import SignUp from "../components/auth/SignUp";
 import SignIn from "../components/auth/SignIn";
-
+import BodyOne from "../components/typography/BodyOne";
 export default {
   name: "Auth",
   components: {
     SignUp,
-    SignIn
+    SignIn,
+    BodyOne
   },
   data() {
     return {
