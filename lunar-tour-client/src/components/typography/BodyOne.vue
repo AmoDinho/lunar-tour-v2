a56fe94d37cf22e67fd5b9a7ad4e8e3f1a89a420
AmoDinho <u14284783@tuks.co.za>
2020-04-12 22:01:39 +0200

    made body a solt

--- lunar-tour-client/src/components/typography/BodyOne.vue
+++ lunar-tour-client/src/components/typography/BodyOne.vue
@@ -1,13 +1,10 @@
 <template>
   <p class="text-sm font-display text-bold text-black">
-    {{ text }}
+    <slot> </slot>
   </p>
 </template>
 <script>
 export default {
-  name: "BodyOne",
-  props: {
-    text: String
-  }
+  name: "BodyOne"
 };
 </script>
