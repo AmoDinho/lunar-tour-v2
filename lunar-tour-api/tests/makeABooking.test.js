ad536d4fa3c6de0c3f3caf4381fe96ac04e5c434
AmoDinho <u14284783@tuks.co.za>
2020-04-24 19:52:21 +0200

    set up the test cases

---
+++ lunar-tour-api/tests/makeABooking.test.js
@@ -0,0 +1,7 @@
+import { makeABooking } from "../src/resolvers/mutation";
+
+describe("Make a booking", () => {
+  test("Successfully able to make a booking", async () => {});
+
+  test("unSuccessfully  able to make a booking", async () => {});
+});
